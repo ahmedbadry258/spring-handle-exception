5d2f287089dd6db6c2516f614029ec820f602bd8
ahmed badry <ahmed.badry258@gmail.com>
2024-11-26 07:13:24 +0200

    add constraint violation exception

---
+++ src/main/java/com/example/demo/entity/ConstraintViolation.java
@@ -0,0 +1,38 @@
+package com.example.demo.entity;
+
+
+
+
+public class ConstraintViolation {
+    
+    private String fieldName;
+    private String message;
+    private String rejectedValue;
+
+    public ConstraintViolation() {
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getRejectedValue() {
+        return rejectedValue;
+    }
+
+    public void setRejectedValue(String rejectedValue) {
+        this.rejectedValue = rejectedValue;
+    }
+}
--- src/main/java/com/example/demo/errors/GlobalException.java
+++ src/main/java/com/example/demo/errors/GlobalException.java
@@ -4,12 +4,18 @@ package com.example.demo.errors;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 
+import com.example.demo.entity.ConstraintViolation;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -25,6 +31,47 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class GlobalException extends ResponseEntityExceptionHandler {
     private static final String BASE_TYPE_URI = "https://egabifsi.com/errors";
 
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ProblemDetail handleUniquenessException(
+            DataIntegrityViolationException exception , WebRequest req) {
+        if(!(exception.getCause() instanceof ConstraintViolationException)) {
+            return handleAllExceptions(exception,req);
+        }
+        ConstraintViolationException constraintViolation =
+                (ConstraintViolationException)exception.getCause();
+        String message = determineConstraintViolationMessage(
+                constraintViolation.getConstraintName());
+        return ProblemDetail.forStatusAndDetail(
+                HttpStatus.BAD_REQUEST, message);
+    }
+
+    private String determineConstraintViolationMessage(String constraintName) {
+        return "SSN must be unique";
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException exception) {
+        ProblemDetail validationProblemDetail =
+                ProblemDetail.forStatusAndDetail(
+                        HttpStatus.BAD_REQUEST, "Validation error");
+
+        List<ConstraintViolation> errors = exception.getFieldErrors()
+                .stream()
+                .map(violation -> getConstraintViolation(violation))
+                .collect(Collectors.toList());
+        validationProblemDetail.setProperty("errors", errors);
+        return validationProblemDetail;
+    }
+    private ConstraintViolation getConstraintViolation(FieldError violation){
+        ConstraintViolation cv=new ConstraintViolation() ;
+        cv.setMessage(violation.getDefaultMessage());
+        cv.setFieldName(violation.getField());
+        cv.setRejectedValue(Objects.isNull(violation.getRejectedValue()) ?"null":violation.getRejectedValue().toString());
+       return cv;
+    }
     @ExceptionHandler(FieldException.class)
     public ProblemDetail handleFieldException(FieldException ex, WebRequest req) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage());
@@ -52,12 +99,7 @@ public class GlobalException extends ResponseEntityExceptionHandler {
         return problemDetail;
     }
 
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ProblemDetail handleExceptions(Exception ex, WebRequest req) {
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage());
-        return problemDetail;
-    }
+
 
     //	  @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
@@ -67,5 +109,11 @@ public class GlobalException extends ResponseEntityExceptionHandler {
         }
         ErrorResponse error = new ErrorResponse("Validation Failed", details);
         return new ResponseEntity(error, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ProblemDetail handleAllExceptions(Exception ex, WebRequest req) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage());
+        return problemDetail;
     }
 }
